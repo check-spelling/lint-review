f67c459dc698ffc9449d49825072fcf72de89a0d
Josh Soref <jsoref@users.noreply.github.com>
2022-03-29 01:40:44 -0400

    Escape python3 \d / \w
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- tests/tools/test_commitcheck.py
+++ tests/tools/test_commitcheck.py
@@ -25,7 +25,7 @@ class TestCommitCheck(TestCase):
         self.assertEqual(0, len(self.problems), 'Empty pattern does not find issues')
 
     def test_execute_commits__match(self):
-        self.tool.options['pattern'] = '\w+'
+        self.tool.options['pattern'] = r'\w+'
         self.tool.execute_commits(self.fixture_data)
         self.assertEqual(0, len(self.problems), 'Commits that do match are ok')
 
@@ -34,28 +34,28 @@ class TestCommitCheck(TestCase):
         self.assertEqual(0, len(self.problems), 'Commits that do match are ok')
 
     def test_execute_commits__no_match(self):
-        self.tool.options['pattern'] = '\d+'
+        self.tool.options['pattern'] = r'\d+'
         self.tool.execute_commits(self.fixture_data)
         self.assertEqual(1, len(self.problems), 'Commits that do not match cause errors')
         msg = (
             'The following commits had issues. '
-            'The pattern \d+ was not found in:\n'
+            'The pattern \\d+ was not found in:\n'
             '* 6dcb09b5b57875f334f61aebed695e2e4193db5e\n')
         expected = IssueComment(msg)
         self.assertEqual(expected, self.problems.all()[0])
 
     def test_execute_commits__custom_message(self):
-        self.tool.options['pattern'] = '\d+'
+        self.tool.options['pattern'] = r'\d+'
         self.tool.options['message'] = 'You are bad.'
         self.tool.execute_commits(self.fixture_data)
         self.assertEqual(1, len(self.problems), 'Commits that do not match cause errors')
-        msg = ('You are bad. The pattern \d+ was not found in:\n'
+        msg = ('You are bad. The pattern \\d+ was not found in:\n'
                '* 6dcb09b5b57875f334f61aebed695e2e4193db5e\n')
         expected = IssueComment(msg)
         self.assertEqual(expected, self.problems.all()[0])
 
     def test_execute_commits__ignore_author_email(self):
         self.tool.author = 'support@github.com'
-        self.tool.options['pattern'] = '\d+'
+        self.tool.options['pattern'] = r'\d+'
         self.tool.execute_commits(self.fixture_data)
         self.assertEqual(0, len(self.problems))
